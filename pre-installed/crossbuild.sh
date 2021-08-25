eda62456fb0d3679b92d493afd9a387e78933eb0
hamlet4401 <38521027+hamlet4401@users.noreply.github.com>
2021-08-25 16:52:53 +0200

    Update to 64bit version python3.9

--- pre-installed/crossbuild.sh
+++ pre-installed/crossbuild.sh
@@ -1,2 +1,2 @@
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker build -t regzon/opencv-python-arm:4.0.0-py3.5 -f Dockerfile-crossbuild .
+docker build -t regzon/opencv-python-arm:4.5.3-py3.9 -f Dockerfile-crossbuild .
